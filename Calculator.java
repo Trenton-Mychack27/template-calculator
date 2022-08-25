f2facf25663137ea13c0611d75d7c103c632f299
Ninad P.Chaudhari <ninadpchaudhari@gmail.com>
2022-08-25 14:38:03 -0400

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -23,20 +23,20 @@ public class Calculator
 
     public int addition(int x, int y) 
     { 
-       return x + y; 
+       //return x + y; 
     } 
     public int subtraction(int x, int y) 
     { 
-       return x - y; 
+       //return x - y; 
     } 
     public int multiplication(int x, int y) 
     {     
-       return x * y; 
+       //return x * y; 
     } 
     public int division(int x, int y) 
     { 
-       solution = x / y; 
-       return solution; 
+       //solution = x / y; 
+       //return solution; 
     } 
 
     public void calc(int ops){
