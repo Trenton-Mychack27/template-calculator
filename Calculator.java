72d857fd86700123e9d56a81a81dd1436ba05b11
Donnie <d30mysl@siena.edu>
2025-09-09 15:54:31 -0400

    changed mult to first

--- Calculator.java
+++ Calculator.java
@@ -21,19 +21,17 @@ public class Calculator
         Scanner operands = new Scanner(System.in); 
     } 
 
-    public int subtraction(int x, int y) 
-    { 
-       //return x - y; 
+    public int multiplication(int x, int y) 
+    {     
+       //return x * y; 
     } 
-    
     public int addition(int x, int y) 
     { 
        //return x + y; 
     } 
-    
-    public int multiplication(int x, int y) 
-    {     
-       //return x * y; 
+    public int subtraction(int x, int y) 
+    { 
+       //return x - y; 
     } 
     public int division(int x, int y) 
     { 
